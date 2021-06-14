d18fef28f3e66dadec69875edfb71d50c12ce963
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-14 18:27:27 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 
 router.post('/register', async (req, res, next) => {
-  console.log('endpoint register wired!')
+  res.json('endpoint register wired!')
 })
 router.post('/login', async (req, res, next) => {
-  console.log('endpoint login wired!')
+  res.json('endpoint login wired!')
 })
 router.get('/logout', async (req, res, next) => {
-  console.log('endpoint logout wired!')
+  res.json('endpoint logout wired!')
 })
 
 module.exports = router
