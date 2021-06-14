050a80e70a928b4fce7116e5503f2452c84bce49
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-14 18:47:49 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,7 +3,11 @@ const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res, next) => {
   try {
-    
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(
+      password,
+      
+    )
   } catch (err) {
     next(err)
   }
