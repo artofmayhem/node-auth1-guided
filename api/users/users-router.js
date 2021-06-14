53b16abd92cc83f67f5e07e61fc98a5a1229b4c7
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-14 18:20:01 +0200

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const { restricted } = require('../auth/auth-middleware');
 const Users = require("./users-model.js");
 
-router.get("/", (req, res, next) => {
+router.use(res)
+
+router.get("/", restricted, (req, res, next) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
