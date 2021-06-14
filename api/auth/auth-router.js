85ec83a181d2160c108de5aeebbea58c8f905a0b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-14 20:07:26 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -36,7 +36,9 @@ router.post('/login', async (req, res, next) => {
 })
 router.get('/logout', async (req, res, next) => {
   if (req.session.user) {
-    req.session.destroy()
+    req.session.destroy(err => {
+      if (err) next({ message})
+    })
   } else {
     res.json({
       message: 'excuse me, do I know you?'
