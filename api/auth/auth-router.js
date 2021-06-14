4a09a349a895df0057621d0ea11578bfc68f7e38
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-14 18:25:01 +0200

    wip

---
+++ api/auth/auth-router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+router.verb('/', async (req, res, next) => {
+  
+})
+router.verb('/', async (req, res, next) => {
+  
+})
+router.verb('/', async (req, res, next) => {
+  
+})
+
+module.exports = router
